8bf3d440435c9bcff9a14c32a45f34f33e06bd21
Alex-Asim <57575430+Alex-Asim@users.noreply.github.com>
2023-11-24 23:18:30 +0200

    Create directories_app.java

---
+++ Examples_Revision/directories_app.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.Arrays;;
+
+public class directories_app {
+
+    public static void main(String[] args) {
+        File businessBooksDirectory = new File("Ebooks\\Business");
+
+        // Make directory
+        if (businessBooksDirectory.mkdirs()) {
+            System.out.println("The new directory is created.");
+        }
+        else{
+            System.out.println("Can't create the directory");
+        }
+
+        businessBooksDirectory.deleteOnExit();
+
+        File file_handlingDirectory = new File("D:\\Αρχεία\\TechPro Academy\\Εργασίες\\Εργασίες Νάσου\\Examples_Revision\\");
+        System.out.println("Files int the handling file directory:");
+        System.out.println(Arrays.toString(file_handlingDirectory.list()));
+        System.out.println(Arrays.toString(file_handlingDirectory.listFiles()));
+
+    }
+}
