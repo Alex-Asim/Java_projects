f917244de8b2cd4caad9502cfe18571f181f1f0f
Alex-Asim <57575430+Alex-Asim@users.noreply.github.com>
2023-11-25 00:27:56 +0200

    Create fileReaderApp.java

---
+++ Examples_Revision/fileReaderApp.java
@@ -0,0 +1,19 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class fileReaderApp {
+    public static void main(String[] args) throws IOException {
+        String HAMLET_PART_FILE_PATH = "D:\\Αρχεία\\TechPro Academy\\Εργασίες\\Εργασίες Νάσου\\Examples_Revision\\Ebooks\\Business.txt";
+
+        // In order to display the first 100 characters from the file you have choose.
+        char [] firstHundredCharacters = new char[100];
+
+        try (FileReader hamletReader = new FileReader(HAMLET_PART_FILE_PATH)){
+            hamletReader.read(firstHundredCharacters);   
+            System.out.println(firstHundredCharacters); 
+            System.out.println("Read more");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
